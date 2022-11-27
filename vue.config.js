06505ff3d6b6d8cbd3886134a7810c344c13b9b5
wutianliang <wutianliang@tanmarket.cn>
2022-11-27 17:50:31 +0800

    update electron and electron-builder version

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-    }
-  }
-})
+      chainWebpackMainProcess: (config) => {
+        config.output.filename("background.js");
+      },
+    },
+  },
+});
